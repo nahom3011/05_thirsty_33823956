60af8f930cfa04f14541c089fe982551fa9d097e
Nbekele <nbeke001@gold.ac.uk>
2025-10-30 23:42:05 +0000

    initial version

---
+++ routes/main.js
@@ -0,0 +1,52 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+//Define our data
+var shopData = {shopName: "The Final Draft", 
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"], 
+                shopInfo: [
+                    {
+                        locations: "Fulham Broadway",
+                        manager: "Dennis Richard",
+                        address: "22 Summer Road, SW6 9JG"
+                    }]
+                };
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData)
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+}); 
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs", shopData)
+});
+
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+ });
+
+ router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+ 
+router.post("/registered", (req,res) => { 
+  res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!. ' + 'We will send an email to you at ' + req.body.email);
+});
+
+// Survey page
+router.get("/survey", (req, res) => {
+  res.render("survey.ejs", shopData);
+});
+
+router.post("/submitted", (req,res) => {
+    res.send(req.body.first + req.body.last + req.body.email + req.body.age + req.body.student)
+})
+
+ // Export the router object so index.js can access it
+module.exports = router;
